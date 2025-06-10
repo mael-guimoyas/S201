50556e9d1013e0b898d995876a4f6c0e0f712110
Mael Guimoyas <Mael.Guimoyas@etu.univ-smb.fr>
2025-06-10 17:49:40 +0200

    client marche

--- S201/MainWindow.xaml.cs
+++ S201/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,15 +15,36 @@ namespace S201
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    /// 
     public partial class MainWindow : Window
     {
+        public ListeCommande LesCommandes { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            ChargeData();
             Conteneur.Content = new Accueuil();
         }
 
         private void ButCom_Click(object sender, RoutedEventArgs e)
+        public void ChargeData()
+        {
+            try
+            {
+                LesCommandes = new ListeCommande("liste principale");
+                this.DataContext = LesCommandes;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur : " + ex.Message, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
             Conteneur.Content = new voirCommandes();
         }
