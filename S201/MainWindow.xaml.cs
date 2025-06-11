5469b49a9753e92fd20419cab72be3ccb218a059
Matthieu Negri <Matthieu.negri@etu.univ-smb.fr>
2025-06-11 14:40:16 +0200

    couleurs bouton

--- S201/MainWindow.xaml.cs
+++ S201/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ namespace S201
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -26,16 +27,35 @@ namespace S201
         private void ButComm_Click(object sender, RoutedEventArgs e)
         {
             Conteneur.Content = new voirCommandes();
+            ButComm.Background = new SolidColorBrush(Color.FromRgb(25, 118, 210));
+            ButComm.Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+            ButAccueil.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+            ButAccueil.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+            ButClients.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+            ButClients.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+
         }
 
         private void ButAccueil_Click(object sender, RoutedEventArgs e)
         {
             Conteneur.Content = new Accueuil();
+            ButComm.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+            ButComm.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+            ButAccueil.Background = new SolidColorBrush(Color.FromRgb(25, 118, 210));
+            ButAccueil.Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+            ButClients.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+            ButClients.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
         }
 
         private void ButClients_Click(object sender, RoutedEventArgs e)
         {
             Conteneur.Content = new GererClients();
+            ButComm.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+            ButComm.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+            ButAccueil.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+            ButAccueil.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+            ButClients.Background = new SolidColorBrush(Color.FromRgb(25, 115, 210));
+            ButClients.Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
         }
     }
 }
