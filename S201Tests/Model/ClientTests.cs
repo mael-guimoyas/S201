bf02e0cac41b9dc68cf2bbe31eec1abe95cbb48d
Jarod Muneret <Jarod.Muneret@etu.univ-smb.fr>
2025-06-13 15:20:52 +0200

    dd

---
+++ S201Tests/Model/ClientTests.cs
@@ -0,0 +1,34 @@
+﻿using S201;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S201.Tests
+{
+    [TestClass()]
+    public class ClientTests
+    {
+        
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+
+
+
+
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            
+        }
+
+
+    }
+}
